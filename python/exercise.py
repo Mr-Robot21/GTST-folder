208554eccfeb4b3c5201df6ab1126626b0110728
Mr Robot <joelmengstab30@gmail.com>
2024-04-24 14:29:53 +0300

    python exercise codes

---
+++ python/exercise.py
@@ -0,0 +1,22 @@
+# Question 1 
+gtst = "Day 7" 
+
+# Question 2
+print ("Hello Today is our" , gtst , "course")   
+print (f"Hello Today is our {gtst} course")        
+
+# Question 3
+even_number = [0, 2, 4] 
+
+print ("The 1st Even number is :" , even_number[0])
+print ("The 2nd Even number is :" , even_number[1])
+print ("The 3rd Even number is :" , even_number[2])
+
+# Question 4 
+fruits = {"apple":10 , "banana":15 , "pineapple":20}
+
+# Question 5
+
+choice = input ("Enter your fruit choice : ")
+
+print(f"The value of {choice} is :" , fruits[choice] , "birr")
---
+++ python/practice1.py
@@ -0,0 +1,8 @@
+
+print("Welcome GTST to Python")   # This is question 1
+print ("Programming is Fun!")   # This is question 2
+print ("Hackers can code.")      # This is question 2
+print ('Welcome GTST' , 'to python' , sep = ',')
+
+print ('New year' , 2023 , 'See you soon!' , sep = '.')
+print ('Good Morning!' , end = ' .')
