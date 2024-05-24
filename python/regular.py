b3d13c61844124b848359a35e879ca6800a7562f
Mr Robot <joelmengstab30@gmail.com>
2024-05-25 00:09:25 +0300

    update

--- python/regular.py
+++ python/regular.py
@@ -1,6 +1,15 @@
-john@gmail.com
-natanhailu@yahoo.com
-micky43@geezetecg.net
-rexder@gmail.com
-Hello there this is me
-My phone number 09
+c = 843044897663847841476319711639772861390329326681532977209935413827620909782846667
+n = 1422450808944701344261903748621562998784243662042303391362692043823716783771691667
+e = 65537
+p = 2159947535959146091116171018558446546179
+q = 658558036833541874645521278345168572231473
+
+# Calculate ph
+t = (p-1)*(q-1)
+
+# Calculate d
+d = pow(e, -1, t)
+
+# Decrypt the ciphertext
+p = pow(c, d, n)
+print(bytearray.fromhex(hex(p)[2:]).decode('ascii'))  
